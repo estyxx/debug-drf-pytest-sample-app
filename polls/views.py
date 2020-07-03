471b3ce6d46713f38ae6bfa06fac65075b069ae1
Ester Beltrami <ester.beltrami@digitalis.com>
2020-07-03 14:55:59 +0100

    Fix test

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 
 from polls.models import Question
 from polls.serializers import QuestionSerializer
@@ -17,4 +18,4 @@ class QuestionViewSet(ModelViewSet):
         # questo e' l'errore che dovrebbe dare eccezione
         request.data["id"] = "AttributeError"
 
-        return Response("ok")
+        return Response("ok", status=status.HTTP_201_CREATED)
