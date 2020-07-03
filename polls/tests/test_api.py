7e6d3ecc3474a468db7523bfe9304c12483b9dff
Ester Beltrami <ester.beltrami@digitalis.com>
2020-07-03 16:03:13 +0100

    Better test

--- polls/tests/test_api.py
+++ polls/tests/test_api.py
@@ -4,20 +4,47 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-class TestContactAPI(APITestCase):
-    def test_post_request_can_create_new_entity(self):
+
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+
+    return APIClient()
+
+
+
+class TestDjangoTesting(APITestCase):
+
+
+    @pytest.mark.xfail
+    def test_format_multipart(self):
         data = {"question_text": "Hello world?"}
 
         url = reverse("question-list")
-        resp = self.client.post(url, data=data)
+        resp = self.client.post(url, data=data, format="multipart")
+
+    def test_format_json(self):
+        data = {"question_text": "Hello world?"}
+
+        url = reverse("question-list")
+        resp = self.client.post(url, data=data, format="json")
         assert resp.status_code == status.HTTP_201_CREATED
 
-
 @pytest.mark.django_db
-def test_project_create(api_client,):
+def test_pytest_json(api_client,):
     data = {"question_text": "Hello world?"}
 
     url = reverse("question-list")
     resp = api_client.post(url, data, format="json")
+
+    assert resp.status_code == status.HTTP_201_CREATED
+
+@pytest.mark.django_db
+@pytest.mark.xfail
+def test_pytest_multipart(api_client,):
+    data = {"question_text": "Hello world?"}
+
+    url = reverse("question-list")
+    resp = api_client.post(url, data, format="multipart")
 
     assert resp.status_code == status.HTTP_201_CREATED
